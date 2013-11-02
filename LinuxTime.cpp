6bfb4d8720e1d06706b5fc30a57a87601ab421b4
TheBuzzSaw <thebuzzsaw@gmail.com>
2013-11-02 09:24:55 -0600

    Simplified Linux native time calculation.

--- LinuxTime.cpp
+++ LinuxTime.cpp
@@ -6,10 +6,9 @@ const DateTime GetNativeTime()
     timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
 
-    int64_t nanoseconds = ts.tv_nsec;
-    nanoseconds += ts.tv_sec * 1000000000;
-
-    return DateTime(1970, 1, 1) + TimeSpan::FromNanoseconds(nanoseconds);
+    return DateTime(1970, 1, 1)
+        + TimeSpan::FromSeconds(ts.tv_sec)
+        + TimeSpan::FromNanoseconds(ts.tv_nsec);
 }
 
 static TimeSpan timerBase;
@@ -19,10 +18,8 @@ const TimeSpan RawTimer()
     timespec ts;
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
 
-    int64_t nanoseconds = ts.tv_nsec;
-    nanoseconds += ts.tv_sec * 1000000000;
-
-    return TimeSpan::FromNanoseconds(nanoseconds);
+    return TimeSpan::FromSeconds(ts.tv_sec)
+        + TimeSpan::FromNanoseconds(ts.tv_nsec);
 }
 
 void ResetTimer()
